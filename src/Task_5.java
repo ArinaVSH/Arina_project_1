3d0ac58afeb73a6d285369b30a08fb3ab056b090
ArinaVSH <Arina.Shabalina@appline.ru>
2020-04-20 23:45:29 +0300

    Задача 5: Необходимо вывести на экран таблицу умножения от 1 до 10 для введённого пользователем любого числа. Использовать циклы запрещено.

---
+++ src/Task_5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Task_5 {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int num = in.nextInt();
+        int num1 = num * 1;
+        int num2 = num * 2;
+        int num3 = num * 3;
+        int num4 = num * 4;
+        int num5 = num * 5;
+        int num6 = num * 6;
+        int num7 = num * 7;
+        int num8 = num * 8;
+        int num9 = num * 9;
+        int num10 = num * 10;
+        System.out.println(num +"*1 = " + num1);
+        System.out.println(num +"*2 = " + num2);
+        System.out.println(num +"*3 = " + num3);
+        System.out.println(num +"*4 = " + num4);
+        System.out.println(num +"*5 = " + num5);
+        System.out.println(num +"*6 = " + num6);
+        System.out.println(num +"*7 = " + num7);
+        System.out.println(num +"*8 = " + num8);
+        System.out.println(num +"*9 = " + num9);
+        System.out.println(num +"*10 = " + num10);
+    }
+}
