5db391854b92796bd8b8d350e7aa710b731d4713
ArinaVSH <Arina.Shabalina@appline.ru>
2020-11-08 19:59:45 +0300

    Задача №8: Цикличный вывод данных на экранЗадание Необходимо с помощью цикла вывести на экран числа от 1 до 50.

--- src/Task_8.java
+++ src/Task_8.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
 public class Task_8 {
-    public static void main (String[] args) {
-        int x = 30;
-        int y = 10;
-        if (x == 30) {
-            if (y == 10) {
-                System.out.print("вавава");
-            }
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter < 51) {
+            System.out.println(counter);
+            counter++;
         }
     }
 }
