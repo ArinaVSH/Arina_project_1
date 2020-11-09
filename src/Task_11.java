ae3bb1cd2d0aecb64166d50ea021197729e484d0
ArinaVSH <Arina.Shabalina@appline.ru>
2020-11-09 16:01:19 +0300

    Задача №11: Ввод строки с клавиатурыЗадание Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран.

---
+++ src/Task_11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String input = in.nextLine();
+        System.out.println(input);
+    }
+}
