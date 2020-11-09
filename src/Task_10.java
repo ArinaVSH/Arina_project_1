41241557ed1c58c9cf578bd220f7982c2e215cbf
ArinaVSH <Arina.Shabalina@appline.ru>
2020-11-09 14:20:43 +0300

    Задание №10: Ввод/вывод данных для матрицы Произведите ввод данных с клавиатуры в матрицу, а после этого произведите вывод первой строки матрицы на экран, где каждый элемент умножается на 3. Размерность матрицы задается пользователем.

---
+++ src/Task_10.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Task_10 {
+    public static void main(String[] args) {
+        System.out.println("Задайте количество строк матрицы: ");
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        System.out.println("Задайте количество столбцов матрицы: ");
+        int n = in.nextInt();
+        int[][] matrix = new int[m][n];
+        System.out.println("Заполните матрицу");
+        for (int t = 0; t < m; t++) {
+            for (int i = 0; i < n; i++) {
+                System.out.println("Элемент с индексом №" + t + i + ": ");
+                matrix[t][i] = in.nextInt();
+            }
+        }
+        System.out.println("Вывод матрицы, умноженной на 3, на экран: ");
+        for (int t = 0; t < m; t++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(" " + matrix[t][i] * 3);
+            }
+            System.out.println();
+        }
+    }
+}
